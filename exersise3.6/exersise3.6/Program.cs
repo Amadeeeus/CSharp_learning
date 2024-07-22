575f92e27bcd59aa5d8a88c22d15e3fef1ee7a55
Amadeus <amadeeus@bk.ru>
2024-07-22 17:49:06 +0300

    +еще практическое

---
+++ exersise3.6/exersise3.6/Program.cs
@@ -0,0 +1,25 @@
+﻿int a;
+double b;
+double c;
+double z;
+Console.WriteLine("Введите номер операции: 1 - Сложение, 2 - Вычитание, 3 -Умножение");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите первую цифру");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите вторую цифру");
+c = Convert.ToDouble(Console.ReadLine());
+switch (a)
+{
+    case 1: 
+    z = a + b;
+    Console.WriteLine($"{a} + {b} = {z}");
+    break; 
+    case 2: 
+    z = a - b;
+    Console.WriteLine($"{a} - {b} = {z}");
+    break; 
+    case 3: z= a * b;
+    Console.WriteLine($"{a} * {b} = {z}");
+    break;
+    default: Console.WriteLine("Неправильный номер операции");break;
+}
