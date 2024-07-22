1ca1baac3e6a9203a256164b62c8c0b0cf252c1a
Amadeus <amadeeus@bk.ru>
2024-07-22 17:30:09 +0300

    +практическое

---
+++ exersise 3.5/exersise 3.5/Program.cs
@@ -0,0 +1,17 @@
+﻿double deposit;
+Console.WriteLine("Введите сумму вклада");
+deposit = Convert.ToDouble(Console.ReadLine());
+if(deposit <100)
+{
+    deposit+= deposit *0.05;
+}
+else if(deposit > 100 && deposit < 200)
+{
+    deposit+=deposit *0.07;
+}
+else 
+{
+    deposit+= deposit * 0.10;
+}
+deposit+=15;
+Console.WriteLine($"Cумма депозита + бонус - {deposit}");
