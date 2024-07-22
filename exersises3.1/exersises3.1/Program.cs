23c80a82fb68ca578754417be96105994d85ac24
Amadeus <amadeeus@bk.ru>
2024-07-22 16:49:18 +0300

    Еще 2 упражнения

---
+++ exersises3.1/exersises3.1/Program.cs
@@ -0,0 +1,18 @@
+﻿int a;
+int b;
+Console.WriteLine("Введите 1 число");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 число");
+b = Convert.ToInt32(Console.ReadLine());
+if(a < b)
+{
+    Console.WriteLine("Первое меньше ");
+}
+else if(a > b)
+{
+    Console.WriteLine("Первое больше");
+}
+else
+{
+    Console.WriteLine("Они равны");
+}
---
+++ exersises3.2/exersises3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+Console.WriteLine("Введите число");
+a = Convert.ToInt32(Console.ReadLine());
+if(a == 5 | a == 10)
+{
+  Console.WriteLine("Число равно либо 5, либо 10");
+}
+else
+{
+    Console.WriteLine("Неизвестное число");
+}
