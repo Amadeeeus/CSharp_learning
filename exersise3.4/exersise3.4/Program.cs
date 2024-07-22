e300f3c27a1920e6e62312264dc93a0782a9a863
Amadeus <amadeeus@bk.ru>
2024-07-22 16:59:58 +0300

    еще упражнение

---
+++ exersise3.4/exersise3.4/Program.cs
@@ -0,0 +1,16 @@
+﻿double deposite;
+Console.WriteLine("Введите сумму вклада");
+deposite = Convert.ToDouble(Console.ReadLine());
+if(deposite < 100)
+{
+    deposite += deposite * 0.05;
+}
+else if(deposite > 100 && deposite < 200)
+{
+    deposite += deposite * 0.07;
+}
+else
+{
+    deposite += deposite * 0.10;
+}
+Console.WriteLine($"депозит - {deposite}");
