fcd988df8fd87734049b140139a9e682f05f8030
Amadeus <amadeeus@bk.ru>
2024-08-05 17:44:07 +0300

    Добавление упражнений по Троелсену

---
+++ Troelsen_Exes/Exersise1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+//<<-----------StringBuilder----------------------------->>
+static void FunWithStringBuilder()
+{
+    Console.WriteLine("=> Using the StringBuilder");
+    StringBuilder sb = new("**** Fantastic Games ****");
+    sb.Append("\n");
+    sb.AppendLine("Half Life");
+    sb.AppendLine("Morrowind");
+    sb.AppendLine("Deus Ex" + "2");
+    sb.AppendLine("System Shock");
+    Console.WriteLine(sb.ToString());
+    sb.Replace("2", " Invisible War");
+    Console.WriteLine(sb.ToString());
+    Console.WriteLine("sb has {0} chars.", sb.Length);
+    Console.WriteLine();
+}
+
+FunWithStringBuilder();
---
+++ Troelsen_Exes/Troelsen_TypeConversations/Troelsen_TypeConversations/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+//<<-------------Неявный перевод short в int --------------->>
+Console.WriteLine("**** Fun with type conversations ****");
+short numb1= 9, numb2 =10;
+Console.WriteLine("{0} + {1} = {2}",numb1,numb2, Add(numb1, numb2));
+Console.ReadLine();
+static int Add(int x, int y)
+{
+    return x+y;
+}
+//<<<--------Превышение обьема short----------->>>
+Console.WriteLine("**** Fun with type conversations ****");
+short numb3= 30000, numb4 =30000;
+//short answer = Add(numb3,numb4);<-- сужающая операция не получилась из за того что max short = 32768;
+Console.WriteLine("{0} + {1} = {2}",numb3,numb4, Add(numb3,numb4));
+Console.ReadLine();
+//<------------------------Еще попытка сужения------------------>
+static sbyte NarrowingAttempt()
+{
+    sbyte myByte = 0;
+    int MyInt = 200;
+    myByte = (sbyte)MyInt; //не получилось int в byte, потому что byte = 127
+    return myByte; 
+}
+//<----------явное приведение типов------------->
+Console.WriteLine("****Fun with type conversion ****");
+short num1 = 30000, num2 = 30000;
+short answer = (short)Add(num1, num2);
+Console.ReadLine();
+Console.WriteLine("{0} + {1} = {2} или {3}", num1,num2,answer,NarrowingAttempt());
