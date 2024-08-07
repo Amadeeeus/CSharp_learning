09da8cf84e5a1c12ba5a6dc6656ac8e6c62a04a7
Amadeus <amadeeus@bk.ru>
2024-08-07 13:00:10 +0300

    Тестовые примеры IoC, DI, Рефлексии

---
+++ Reflection_Libraly/Reflection_Libraly/Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace Reflection_Libraly
+{
+    public class Human
+    {
+        public Human(string Message)
+        {
+          string message =  Message;
+          Console.WriteLine($"{Message}");
+        }
+
+        
+    }
+}
---
+++ test_reflection_GPT/test_reflection_GPT/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] numbers = {1,2,3};
+Type type = numbers.GetType();
+Console.WriteLine($"Type: {type.FullName}\n{type.GetElementType()}");
+foreach(var assembly in AppDomain.CurrentDomain.GetAssemblies())
+{
+    Console.WriteLine($"Assembly: {assembly.FullName}");
+    foreach(var type1 in assembly.GetTypes())
+    {
+       Console.WriteLine($" Type: {type.FullName}");
+    }
+} 
