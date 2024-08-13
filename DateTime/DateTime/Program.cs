fc8c948ec2a7bf06d3be2e27c613031da40e265f
Amadeus <amadeeus@bk.ru>
2024-08-13 12:56:55 +0300

    LINQ+ Async_Practice

--- DateTime/DateTime/Program.cs
+++ DateTime/DateTime/Program.cs
@@ -12,7 +12,7 @@ await GetTime();
 
 static async Task GetTime()
 { 
-  await dateTime();
+  await Task.WhenAll(dateTime(),GetCoupleDays(),GetYears(),Timestamp(),OtherTimestamp());
   
 }
 
@@ -28,8 +28,56 @@ static async Task dateTime()
         Console.WriteLine(hours);
         hours++;
         Console.WriteLine(date);
-        Thread.Sleep(200);
+        await Task.Delay(200);
     }
     await Task.Delay(1000);
+}
+
+static async Task GetCoupleDays()
+{   double days = 1;
+    DateTime date = new();
+    date = DateTime.Now;
+    for(int i = 0; i<7; i++){
+    date = date.AddDays(days);
+    days++;
+    Console.WriteLine(date);
+    await Task.Delay(1000);
+    }
+}
+
+static async Task GetYears()
+{
+    DateTime date =  new();
+    int years = 1;
+    date = DateTime.Now;
+    for(int i=0; i<10; i++)
+    {
+        date = date.AddYears(years);
+        years++;
+        Console.WriteLine(date);
+        await Task.Delay(200);
+    }
+}
+
+
+static async Task Timestamp()
+{ double date = ConvertToUnix(DateTime.Now);
+  Console.WriteLine($"Unix timestamp - {date}");
+  await Task.Delay(200); 
+}
+
+static long ConvertToUnix(DateTime date)
+{
+
+    return (long)(date - new DateTime(1970,1,1)).TotalSeconds;
+}
+
+
+static async Task OtherTimestamp()
+{
+  DateTime date  = DateTime.Now;
+  long unixtime = ((DateTimeOffset)date).ToUnixTimeSeconds();
+  Console.WriteLine($"Unix timestamp = {unixtime}");
+  await Task.Delay(200);
 }
 }
---
+++ Returned_TaskT/Returned_TaskT/Program.cs
@@ -0,0 +1,29 @@
+﻿class Program
+{
+    static async Task Main(string[] agns)
+    {
+        var list = new List<string>();
+        string[] people = {"Tom","Bob", "kyle","Mike","Tim"};
+        list = await SortedArray(people);
+        foreach(string names in list)
+        {
+            Console.WriteLine($"Names: {names}");
+        }
+        
+    }
+
+    static async Task<List<string>> SortedArray(string[] people)
+    {
+        var selected = new List<string>();
+        foreach(string peoples in people)
+        {
+            if(peoples.ToUpper().StartsWith("T"))
+            {
+                selected.Add(peoples);
+            }
+        }
+        selected.Sort(); 
+        await Task.Delay(200);
+        return selected;
+    }
+}
