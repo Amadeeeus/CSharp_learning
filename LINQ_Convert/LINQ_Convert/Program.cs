4fe5913a095e426408dec89f350fc63cd0fb94ce
Amadeus <amadeeus@bk.ru>
2024-08-14 15:21:50 +0300

    14.08.24 15:22

---
+++ LINQ_Convert/LINQ_Convert/Program.cs
@@ -0,0 +1,6 @@
+﻿List<string> stringsNumbers = new List<string>{"1","2","3","4"};
+List<int>intNumbers = stringsNumbers.Select(int.Parse).ToList();
+foreach(var num in intNumbers)
+{
+    Console.WriteLine(num);
+}
